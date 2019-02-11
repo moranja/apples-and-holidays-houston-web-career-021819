b8cf8192d7d944ae53cadc5e257032db4b83a93a
Adam Moran <jeffadammoran@gmail.com>
2019-02-11 20:32:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_supplies_in_holidays(holiday_hash)
         
         counter_array.collect do |i|
           next_value = i + 1
-          binding.pry
+          #binding.pry
           fix_underscores[i] = " "
           fix_underscores[next_value] = fix_underscores.upsize
         end
