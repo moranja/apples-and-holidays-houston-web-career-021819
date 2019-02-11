74195ce8e8b8aee333faec128e98ec2d817f6593
Adam Moran <jeffadammoran@gmail.com>
2019-02-11 20:39:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  new_string = ""
+  #new_string = ""
   holiday_hash.each do |season, hash|
     puts "#{season.capitalize}:"
     hash.each do |holiday, stuff|
